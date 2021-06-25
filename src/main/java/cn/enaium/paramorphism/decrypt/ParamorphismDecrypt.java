41f57fd74305489b902c7f7d83b338e34db6a402
Enaium <32991121+Enaium@users.noreply.github.com>
2021-06-25 21:09:59 +0800

    Update ParamorphismDecrypt.java

--- src/main/java/cn/enaium/paramorphism/decrypt/ParamorphismDecrypt.java
+++ src/main/java/cn/enaium/paramorphism/decrypt/ParamorphismDecrypt.java
@@ -89,10 +89,8 @@ public class ParamorphismDecrypt extends JFrame {
                         name = jarEntry.getName();
                     } else if (jarEntry.getName().endsWith(".class/")) {
                         name = jarEntry.getName().substring(0, jarEntry.getName().length() - 1);
-                    } else if (jarEntry.isDirectory()) {
-                        continue;
                     } else {
-                        name = jarEntry.getName();
+                        continue;
                     }
                     jarOutStream.putNextEntry(new ZipEntry(name));
                     jarOutStream.write(jarFile.getInputStream(jarEntry).readAllBytes());
